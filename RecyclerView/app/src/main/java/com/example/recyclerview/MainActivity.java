5794cd220478449ed60bb70d0fbdfa51a2b4fc1d
RodrigoMiguelBaptist <37610@esjaloures.og>
2026-03-17 15:39:12 +0000

    Implementar recyclerView

--- RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recyclerview.models.SuperHero;
 
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<SuperHero> lista;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         this.buildSuperHeroes();
+
+        this.recyclerView = findViewById(R.id.recyclerView);
     }
 
     private void buildSuperHeroes(){
